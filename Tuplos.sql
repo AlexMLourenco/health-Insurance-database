908858fc3af590cebb8305b19168483ca5dfa4ef
Alexandre Lourenço <alexandre.lourenco@ua.pt>
2019-06-03 15:15:18 +0100

    added one trigger

--- Tuplos.sql
+++ Tuplos.sql
@@ -1,30 +1,30 @@
 USE [SeguradoraSaude]
 GO
 
-INSERT INTO [dbo].[Pessoa] ([Nome],[NIF] ,[Morada] ,[Idade] ,[Sexo])
+INSERT INTO [SeguradoraSaude].[Pessoa] ([Nome],[NIF] ,[Morada] ,[Idade] ,[Sexo])
      VALUES ('Alexandre Lourenco','15174922','Rua Sao Tiago','22','M')
          
-INSERT INTO [dbo].[Pessoa] ([Nome],[NIF] ,[Morada] ,[Idade] ,[Sexo])
+INSERT INTO [SeguradoraSaude].[Pessoa] ([Nome],[NIF] ,[Morada] ,[Idade] ,[Sexo])
      VALUES ('Manuel Augusto','11922355','Rua Tivoli', '61','M')
 
-INSERT INTO [dbo].[Pessoa] ([Nome],[NIF] ,[Morada] ,[Idade] ,[Sexo])
+INSERT INTO [SeguradoraSaude].[Pessoa] ([Nome],[NIF] ,[Morada] ,[Idade] ,[Sexo])
      VALUES ('Ana Lopes','34762323','Avenida da Liberdade','26','F')
 GO
 
-INSERT INTO [dbo].[Cliente]
+INSERT INTO [SeguradoraSaude].[Cliente]
            ([NIFCliente])
      VALUES
            ('15174922')
 GO
 
-INSERT INTO [dbo].[ClinicaHospitalar]
+INSERT INTO [SeguradoraSaude].[ClinicaHospitalar]
            ([NomeClinica]
            ,[Localizacao])
      VALUES
            ('Clinica da Boa Saude','Porto')
 GO
 
-INSERT INTO [dbo].[Medico]
+INSERT INTO [SeguradoraSaude].[Medico]
            ([NIFMedico]
            ,[Ordenado]
            ,[Especializacao]
@@ -33,7 +33,7 @@ INSERT INTO [dbo].[Medico]
            ('11922355',2500,'Cardiologia',1)
 GO
 
-INSERT INTO [dbo].[Secretaria]
+INSERT INTO [SeguradoraSaude].[Secretaria]
            ([NIFSecretaria]
            ,[Ordenado])
      VALUES
@@ -41,7 +41,7 @@ INSERT INTO [dbo].[Secretaria]
 GO
 
 
-INSERT INTO [dbo].[Consulta]
+INSERT INTO [SeguradoraSaude].[Consulta]
            ([dataConsulta]
            ,[hora]
            ,[NIFCliente]
@@ -53,7 +53,7 @@ INSERT INTO [dbo].[Consulta]
            
 GO
 
-INSERT INTO [dbo].[Pagamento]
+INSERT INTO [SeguradoraSaude].[Pagamento]
            ([RefPagamento]
            ,[MetodoPagamento]
            ,[Codigo]
@@ -64,7 +64,7 @@ INSERT INTO [dbo].[Pagamento]
            ('505243456','Numerario','1234','20','2019-05-3','34762323')
 GO
 
-INSERT INTO [dbo].[Ficha]
+INSERT INTO [SeguradoraSaude].[Ficha]
            ([RelatorioDiagnostico]
            ,[ConsultaInternamento]
            ,[NumConsulta]
@@ -74,7 +74,7 @@ INSERT INTO [dbo].[Ficha]
            ('R','C','1','15174922','505243456')
 GO
 
-INSERT INTO [dbo].[FichaDoencas]
+INSERT INTO [SeguradoraSaude].[FichaDoencas]
            ([ID]
            ,[NIFCliente]
            ,[TipoDoenca]
@@ -85,7 +85,7 @@ INSERT INTO [dbo].[FichaDoencas]
            ('1','15174922','Mononucleose','1','2018-06-6',1)
 GO
 
-INSERT INTO [dbo].[Seguro]
+INSERT INTO [SeguradoraSaude].[Seguro]
            ([ID]
            ,[Tipo]
            ,[Cota]
@@ -96,7 +96,7 @@ INSERT INTO [dbo].[Seguro]
 GO
 
 
-INSERT INTO [dbo].[ClienteTemSeguro]
+INSERT INTO [SeguradoraSaude].[ClienteTemSeguro]
            ([NIFCliente]
            ,[ID])
      VALUES
---
+++ triggers.sql
@@ -0,0 +1,47 @@
+-- Trigger for getting insurece payment
+GO
+CREATE TRIGGER SeguradoraSaude.TrgInsurenceCover ON SeguradoraSaude.Pagamento
+INSTEAD OF INSERT
+AS 
+BEGIN
+    DECLARE @refPay INT;
+    DECLARE @method INT;
+    DECLARE @dataPay DATE;
+    DECLARE @dataInsurence DATE;
+    DECLARE @value INT;
+    DECLARE @NIFClient INT;
+    DECLARE @NIFSecre INT;
+    DECLARE @carencia INT;
+    DECLARE @code INT;
+    --SET @currentData = GETDATE();
+    
+    SELECT @dataPay = DataPagamento -- payment Date
+    FROM SeguradoraSaude.Pagamento AS PAY
+    WHERE PAY.RefPagamento=@refPAy;
+    
+    SELECT @dataInsurence = DataSeguro -- insurence Date
+    FROM SeguradoraSaude.Seguro AS S
+    JOIN SeguradoraSaude.ClienteTemSeguro AS CTS
+    ON S.ID=CTS.ID
+    WHERE CTS.NIFCliente=@NIFClient;
+    
+    SELECT @carencia = DataSeguro -- periodo carencia
+    FROM SeguradoraSaude.Seguro AS S
+    JOIN SeguradoraSaude.ClienteTemSeguro AS CTS
+    ON S.ID=CTS.ID
+    WHERE CTS.NIFCliente=@NIFClient;
+    
+    IF(DATEDIFF(MONTH, @dataPay,@dataInsurence) > @carencia)
+    BEGIN
+        PRINT 'Despesas pagas na totalidade pelo seguro!';
+    END
+    
+    ELSE
+
+    BEGIN
+        PRINT 'O seguro não cobre as despesas médicas!';
+        INSERT INTO [SeguradoraSaude].Pagamento
+        VALUES (@refPay, @method, @code, @value, @dataPay, @NIFSecre);
+    END
+
+END
