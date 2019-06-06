8770d52483b2efdb8f762d14f144ba1e109f1815
Alexandre Lourenço <alexandre.lourenco@ua.pt>
2019-06-06 22:50:46 +0100

    index's corrected

--- index.sql
+++ index.sql
@@ -1,23 +1,11 @@
 GO
--- Indexar Clientes pelo NIF (chave única)
-CREATE UNIQUE INDEX IxCNIF ON SeguradoraSaude.Cliente(NIFCliente)
+-- Indexar Pessoas pelo nome
+CREATE INDEX IxPNome ON SeguradoraSaude.Pessoa(Nome)
 
 GO
--- Indexar Medicos pelo NIF (chave única)
-CREATE UNIQUE INDEX IxMNIF ON SeguradoraSaude.Medico(NIFMedico)
+-- Indexar Pessoas pela morada
+CREATE UNIQUE INDEX IxPMor ON SeguradoraSaude.Pessoa(Morada)
 
 GO
--- Indexar Secretarias pelo NIF (chave única)
-CREATE UNIQUE INDEX IxSNIF ON SeguradoraSaude.Secretaria(NIFSecretaria)
-
-GO
--- Indexar Clinicas pelo numero de clínica (chave única)
-CREATE UNIQUE INDEX IxClinc ON SeguradoraSaude.ClinicaHospitalar(NumClinica)
-
-GO
--- Indexar Pagamentos pela respetiva referencia de pagamento (chave única)
-CREATE UNIQUE INDEX IxRefPay ON SeguradoraSaude.Pagamento(RefPagamento)
-
-GO
--- Indexar Seguros pelo numero identificador (chave única)
-CREATE UNIQUE INDEX IxIDS ON SeguradoraSaude.Seguro(ID)
+-- Indexar clínicas por localização
+CREATE UNIQUE INDEX IxCLL ON SeguradoraSaude.ClinicaHospitalar(Localizacao)
